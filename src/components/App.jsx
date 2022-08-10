e201caf5c482112f1abf50f21fcfbe98999b418c
Abigailal <abigail_al@outlook.com>
2022-08-10 13:07:34 -0500

    [ADD] Config

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/css/app.css';
+
+function App() {
+    return (
+        <div className="Prueba">
+            <h1 className="Prueba__Title">Hola Mundo</h1>
+            <h2>Bienvenido</h2>
+        </div>
+    );
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export { App };
